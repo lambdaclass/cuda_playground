24e961e507c38eea27bf1ae6c1118d5d6cbb8200
ronibarylko <ronibarylko@hotmail.com>
2023-03-21 17:02:56 -0300

    FFT: Add x_in as a param (#3)
    ---------
    
    Co-authored-by: Tobias Valdes Castro <tobini2@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 #[link(name = "fft", kind = "static")]
 extern "C" {
-    fn main_fft();
+    fn main_fft(x_in: *mut i32, n: usize);
 }
+
+use rand::prelude::*;
 
 fn main() {
     unsafe {
-        main_fft();
+        let mut rng = rand::thread_rng();
+        let n = 8;
+        let mut x_in: Vec<i32> = Vec::with_capacity(n);
+        for _i in 0..n {
+            x_in.push(rng.gen_range(0..10));
+        }
+        main_fft(x_in.as_mut_ptr(), n);
+        println!("Done with Rust!");
     }
 }
